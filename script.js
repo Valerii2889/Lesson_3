6506cd968283a38e2bf333c7eecdbb87ecb5377a
Valerii2889 <118856034+Valerii2889@users.noreply.github.com>
2023-04-11 01:45:09 +0300

    add lesson_3

--- script.js
+++ script.js
@@ -1,516 +1,321 @@
-// if(){
+//             0    1       2      3        4
+// const array = [1, 'Hello', null, true, [1, 2, 3]];
+//             1    2       3      4        5
+// console.log(typeof array);
+// console.log(Array.isArray(array));
+// Array.isArray(array) - повертає true/false в залежності від того чи ми працюємо з масивом чи ні
+// console.log(array[3]);
+// const first = array[0];
+// console.log(first);
+// const last = array[array.length - 1];
+// console.log(array.length);
+// console.log(last);
 
-// }else{
+// for (let i = 0; i < array.length; i += 1) {
+//     const item = array[i];
+//     if (Array.isArray(item)) {
+//         for (let j = 0; j < item.length; j+=1){
+//             console.log(item[j]);
+//         }
+//         continue;
+//     }
+//     console.log(array[i]);
+// }
+// const array = [1, 'Hello', null, true, [1, 2, 3]];
 
+// for(let item of array){
+//     // console.log(item);
+//     if(typeof item === 'string'){
+//         item = false
+//     }
+// }
+// console.log('for of',array);
+
+// for(let i = 0; i< array.length; i+=1){
+//     if(typeof array[i] === 'string'){
+//         array[i] =false;
+//     }
+// }
+// console.log('for',array);
+
+
+
+// const array = [1, 'Hello', null, true, [1, 2, 3]];
+
+// let i = 0;
+// while (i < array.length) {
+//     if (typeof array[i] === 'string') {
+//         array[i] = false;
+//     }
+//     i += 1;
 // }
 
-// if(4>5){ //4>5 => false
-//     console.log('true');
-// }else{
-//     console.log('false');
-// }
-// const value = 7
-
-// if (value > 5) { // 7> 5 => true
-//     console.log('4>5');
-// } else if (value > 6) {
-//     console.log('4>6');
-// } else if (value > 3) {
-//     console.log('4>3');
-// } else {
-//     console.log('else');
-// }
-
-// const value = '';
-// if (value) { //0 => false
-//     console.log('if');
-// } else {
-//     console.log('else');
-// }
-
-
-// condition ? exmple 1 : example 2
-
-
-// const result = 7 > 6 ? 'більше' : 'меньше';
-
-// let result;
-
-// if (7 > 6) {
-//     result = 'більше';
-// } else {
-//     result = 'меньше';
-// }
-
-// console.log(result);
-
-
-
-// const result = 10 + 5;
-// console.log(result);
-// console.log(testValue);
-// const value = 5;
-// if (value === 1) {
-//     console.log('one');
-// } else if (value === 2) {
-//     console.log('two');
-// } else if (value === 5) {
-//     console.log('Good result');
-// } else {
-//     console.log("default")
-// }
-
-// switch (value) {
-//     case 1:
-//         console.log('one');
-//         break
-//     case 2:
-//         console.log('two');
-//         break
-//     case 5:
-//         console.log('Good result');
-//         break
-//     default:
-//         console.log("default")
-// }
-
-// const a = 'name';
-// switch (a) {
-//     case 'age':
-//         console.log('one');
-//         break;
-//     case 'name':
-//         console.log('two');
-//         break;
-//     case 'city':
-//         console.log('Good result');
-//         break;
-//     default:
-//         console.log("default")
-// }
-
-
-// const let
-
-// var
-
-// 1 Глобальна
-// 2 Блочна
-// 3 Функціональна
+// console.log(array);
 
 
 // 
-// Блочна
-// {
 
-// }
+// let value = 1;
+// let test = value;
+// value='hello';
 
+// console.log('value',value);
+// console.log('test',test);
 
-// Функціональна
-// function(){
 
-// }
 
+// const array = [1, 2, 3, 4, 5];
+// const copy = array;
+// array.push(6, 7);
+// copy.push(8,9)
+// console.log(array === copy);
+// console.log(array);
+// console.log(copy);
 
 
-// const a = 15;
+// console.log(1 === 1);
+// console.log('hello' === 'hello');
+// console.log([1, 2, 3] === [1, 2, 3]);
 
-// if (true) {
-//     console.log(a);
-// }
 
-// if(true){
-//     var b = 22;
-// }
-// console.log(b);
+// let number = 1;
+// number +=15;
+// console.log(number);
 
-// function foo() {
-//     console.log(a);
-//     var b = 22;
-// }
-// console.log(b);
 
+// let array = [1, 2, 3];
+// // array ='string';
 
-// в глобальні
-// let a = 15;
+// array.push(4)
+// // array.pop();
+// // array.length = 10;
+// console.log(array);
 
-// if (true) {
-//     // Блочна
-//     let a = 1;
-//     a = 25;
-// }
+// Перерва до 21.00
 
-// console.log(a);
 
-// глобальна
-// let a = 15;
+// 
+// const str = 'Hello world I`m learning JS';
+// const arr = str.split('-');
+// console.log(arr);
 
-// if (true) {
-//     // звернення до ініціалізації
-//     a = 25;
-//     let a = 1;
-// }
+// // const result = arr.reverse().join(' ')
 
-// console.log(a);
 
-// глобальна
-// let a = 10;
-
-
-// if (true) {
-//     // блочна
-//     a = 15;
-//     if (true) {
-//         a = 25
-//     }
-// }
-// console.log(a);
-
-// if(true){
-//     // блочна
-//     let a = 15;
-//     if(true){
-//         a = 25
-//     }
-// }
-// console.log(a);
-
-// if(true){
-//     // блочна
-//     let a = 15;
-//     if(true){
-//         a = 25
-//     }
-//     console.log(a);
-// }
-
-
-// let a = 15;
-
-
-// if (true) {
-//     // блочна
-//     a = 15;
-//     if (true) {
-//         let a = 25
-//     }
-// }
-// console.log(a);
-
-// let b = 25;
-
-// if (true) {
-//     // блочна
-//     let a = 15;
-//     b = 44;
-//     if (true) {
-//         a = 25
-//         b = 31
-//     }
-//     a = 44
-//     console.log(b);
-// }
-// console.log(a);
-
-
-
-
-
-// for (let i = 0; i < 5; i+=1){
-//     console.log(i);
-// }
-//  12345
-// const str = 'hello world';
-//          //  01234
-// for(let i = 0; i < str.length; i+=1){
-//     console.log(str[i]); //0,1,2,3,4,5,6
-// }
-
-
-
-// while(true){
-//     console.log('hello world');
-// }
-
-
-// const str = 'hello world';
-
-
-// let i = 0;
-// while (i < str.length) {
-//     if(str[i] === 'w'){
-//         console.log('yes');
-//         break;
-//     }
-//     console.log('no', str[i]);
-//     i += 1;
-// }
-
-// const str = 'hello world';
-// let i = 15;
-// do {
-//     if (str[i] === 'w') {
-//         console.log('yes');
-//         break;
-//     }
-//     console.log('no');
-//     i += 1;
-// } while (i < str.length)
-
-
-
-
-
-// Example 1 - Введення користувача та розгалуження
-
-// Використовуючи конструкцію if..else та prompt, напиши код, який питатиме: "Яка офіційна назва JavaScript?". Якщо користувач вводить ECMAScript, то показуй alert з рядком "Правильно!", в іншому випадку - "Не знаєте? ECMAScript!"
-
-// const value = prompt("Яка офіційна назва JavaScript?");
-// if (value === 'ECMAScript') {
-//     alert("Правильно!")
-// } else {
-//     alert("Не знаєте? ECMAScript!")
-// }
-
-
-// Example 2 - Відображення часу (if...else)
-// Напиши скрипт для відображення годин та хвилин у консолі браузера у вигляді рядка формату "14 г. 26 хв.". Якщо значення змінної minutes дорівнює 0, то виводь рядок "14 г.", без хвилин.
-
-// const hours = 14;
-// const minutes = 1;
-// let timestring;
-
-// if (minutes) {
-//     console.log(`${hours} г. ${minutes}хв.`);
-// } else {
-//     console.log(`${hours} г.`);
-// }
-
-
-
-// Example 3 - Розгалуження
-// Напиши скрипт, який виводить у консоль рядок "Це позитивне число", якщо у prompt користувач ввів число більше нуля. Якщо було введено нуль, виводь в консоль рядок "Це нуль". Якщо передали від'ємне число, у консолі повинен бути рядок "Це негативне число".
-
-// const userInput = Number(prompt('Введіть число'));
-// console.log( typeof userInput);
-// if (!userInput) { // 0 =>! + false => true
-//     console.log("Це нуль");
-// } else if (userInput > 0) {
-//     console.log("Це позитивне число");
-// } else {
-//     console.log("Це негативне число");
-// }
-
-// const userInput = Number(prompt('Введіть число'));
-// if (userInput % 2) {
-//     console.log("Не парне");
-// } else {
-//     console.log('Парне');
-// }
-
-
-// console.log(userInput % 2 ? "Не парне" : 'Парне');
-
-// if (0) { //=> false
-
-// } else {
-//     console.log('в мене нуль');
-// }
-
-// if (!0) { //=> false
-//     console.log('в мене нуль');
-// }
-
-
-// Example 4 - Вкладені розгалуження
-// Напиши скрипт, який порівнює числа у змінних a та b. Якщо обидва значення більше 100, то виведи в консоль максимальне з них. В протилежному випадку у консолі повинна бути сума значення b та числа 512.
-
-const a = 120;
-const b = 180;
-// if (a > 100 && b > 100) {
-//     if (a > b) {
-//         console.log(a);
-//     } else {
-//         console.log(b);
-//     }
-// } else {
-//     console.log(b + 512);
-// }
-// if (a > 100 && b > 100) {
-//     console.log(a > b ? a : b);
-// } else {
-//     console.log(b + 512);
-// }
-
-// Example 5 - Форматування посилання (endsWith)
-// Напиши скрипт який перевіряє чи закінчується значення змінної link символом /. Якщо ні, додай до кінця значення link цей символ. Використовуй конструкцію if...else.
-
-// let link = 'https://my-site.com/about';
-// // // Пиши код нижче за цей рядок
-// if(!link.endsWith('/')){
-//     link +='/';
-// }
-// // // Пиши код вище за цей рядок
-// console.log(link);
-
-
-
-// Example 6 - Форматування посилання (includes та логічне «І»)
-// Напиши скрипт який перевіряє чи закінчується значення змінної link символом /. Якщо ні, додай до кінця значення link цей символ, але тільки в тому випадку, якщо в link є підрядок "my-site". Використовуй конструкцію if...else.
-
-// let link = 'https://somesite.com/about';
-// // // Пиши код нижче за цей рядок
-// if(!link.endsWith('/') && link.includes("my-site")){
-//     link +='/';
-// }
-// // // Пиши код вище за цей рядок
-// console.log(link);
-
-
-
-// Example 7 - Форматування посилання (тернарний оператор)
-// Виконай рефакторинг коду задачі номер 4, використовуючи тернарний оператор.
-
-// let link = 'https://my-site.com/about';
-// // if (link.includes('my-site') && !link.endsWith('/')) {
-// //     link += '/';
-// // }
-// console.log(link.includes('my-site') && !link.endsWith('/') ? link += '/' : link);
-
-
-
-// Example 8 - if...else та логічні оператори
-// Напиши скрипт який виводитиме в консоль браузера рядок залежно від значення змінної hours.
-
-// Якщо значення змінної hours:
-
-// менше 17, виводь рядок "Pending"
-// більше або дорівнює 17 і менше або дорівнює 24, виводь рядок "Expires"
-// більше 24 , виводь рядок "Overdue"
-// const hours = 19;
-
-// if (hours < 17) {
-//     console.log("Pending");
-// } else if (hours >= 17 && hours <= 24) {
-//     console.log("Expires");
-// } else {
-//     console.log('Overdue');
-// }
-
-
-// Example 9 - Дедлайн здачі проекту (if...else)
-// Напиши скрипт для відображення часу дедлайну здачі проекту. Використовуй конструкцію if...else.
-
-// Якщо до дедлайну 0 днів - виведи рядок "Сьогодні"
-// Якщо до дедлайну 1 день - виведи рядок "Завтра"
-// Якщо до дедлайну 2 дні - виведи рядок "Післязавтра"
-// Якщо до дедлайну 3+ днів - виведи рядок "Дата у майбутньому"
-// const daysUntilDeadline = 2;
-// // // Пиши код нижче за цей рядок
-// if (!daysUntilDeadline) {
-//     console.log("Сьогодні");
-// } else if (daysUntilDeadline === 1) {
-//     console.log("Завтра");
-// } else if (daysUntilDeadline === 2) {
-//     console.log("Післязавтра");
-// } else {
-//     console.log("Дата у майбутньому");
-// }
-
-
-
-// Example 10 - Дедлайн здачі проекту (switch)
-// Виконай рефакторинг коду задачі номер 5 використовуючи switch.
-
-// const daysUntilDeadline = -1;
-
-// // if (daysUntilDeadline === 0) {
-// //   console.log('Сьогодні');
-// // } else if (daysUntilDeadline === 1) {
-// //   console.log('Завтра');
-// // } else if (daysUntilDeadline === 2) {
-// //   console.log('Післязавтра');
-// // } else {
-// //   console.log('Дата у майбутньому');
-// // }
-
-// if (daysUntilDeadline >= 0) {
-//     switch (daysUntilDeadline) {
-//         case 0:
-//             console.log('Сьогодні');
-//             break;
-//         case 1:
-//             console.log('Завтра');
-//             break;
-//         case 2:
-//             console.log('Післязавтра');
-//             break;
-//         default:
-//             console.log('Дата у майбутньому');
-//     }
-// } else {
-//     console.log("Друже ти проспав");
-// }
-
-
-
-// Example 11 - Цикл for
-// Напиши цикл for, який виводить у консоль браузера числа за зростанням від min до max, але тільки якщо число кратне 5.
-
-// const max = 100;
-// const min = 20;
-// !min => 20 => true => !true=> false
-// // 1 ''
-// // 2 0
-// // 3 null
-// // 4 undefined
-// // 5 NaN
-// // 6 false
-
-
-// const max = 100;
-// const min = 20;
-
-// for (let i = min; i >= min && i <= max; i += 1) {
-//     if (!(25 % 5)) { //25 % 5 => 0 => false => !false => true
-//         console.log(i);
-//     }
-// }
-
-
-
-
-
-
-// Example 12 - Введення користувача та розгалуження
-// Напиши скрипт, який питатиме логін за допомогою prompt та логувати результат у консоль браузера.
-
-// Якщо відвідувач вводить "Адмін", то prompt запитує пароль
-// Якщо нічого не введено або натиснуто клавішу Esc - вивести рядок "Скасовано"
-// В іншому випадку вивести рядок "Я вас не знаю"
-// Пароль перевіряти так:
-// Якщо введено пароль "Я адмін", то вивести рядок "Доброго дня!"
-// Інакше виводити рядок "Невірний пароль"
-
-// const login = prompt("Тут має бути ЛОГІН");
-// if(!login){
-//   console.log("Скасовано");  
-// }else if(login === "Адмін"){
-//     const password = prompt("Тут має бути ПАРОЛЬ")
-//     console.log(password === "Я адмін" ? "Доброго дня!" : "Невірний пароль");
-// }else{
-//     console.log("Я вас не знаю");
-// }
-// console.log(login);
-// const password = prompt()
-
-
-// function add(a, b) {
-//     const result = a + b;
-//     return result;
-// }
-
-// console.log(add(2, 4));
-
-// const str = 'М\'який'
+// const result = str.split(' ').reverse().join(' ')
 // console.log(str);
+// // console.log(arr);
+// console.log(result);
+
+
+
+
+
+// const arr = ['hello', 'wolrd'];
+// const idx = arr.indexOf('wolrd');
+
+// if (arr.indexOf('wolrd') === -1) {
+//     console.log("NO");
+// } else {
+//     console.log('yes');
+// }
+// console.log(idx);
+// console.log(arr.includes('wolrd'));
+
+// if (arr.includes('wolrd')) {
+//     console.log('YES');
+// } else {
+//     console.log('NO');
+// }
+//      3           4
+// const arr = ['hello', 'wolrd'];
+// push pop
+// arr.push(1, 'a',[1,2])
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// console.log(arr);
+
+// shift unshift
+
+//      0   1      2
+// arr.unshift(1, 'a', [1, 2])
+// // arr.shift();
+// // arr.shift();
+// // arr.shift();
+// // console.log(arr);
+
+// // slice
+// const result = arr.slice(1,3);
+// console.log('result',result);
+// console.log('arr',arr);
+
+// const arr = ['hello', 'world', 'js'];
+// // splice
+
+// // arr.splice(1, 1, 'from', 'react', 'node')
+// // console.log(arr);
+// const idx = arr.indexOf('world');
+// const result = arr.splice(idx, 1, 'from')
+// console.log(arr);
+// console.log('result',result);
+
+
+// concat
+
+// const arr = [1, 2, 3];
+// const arr1 = [4, 5];
+// const arr2 = [6, 7];
+// const result = arr2.concat(arr, arr1)
+// console.log(arr);
+// console.log(result);
+
+
+// -----------------------------------//
+
+// Example 1 - Базові операції з масивом
+// Створіть масив genres з елементами «Jazz» та «Blues».
+// Додайте «Рок-н-рол» до кінця.
+// Виведіть у консоль перший елемент масиву.
+// Виведіть у консоль останній елемент масиву. Код повинен працювати для масиву довільної довжини.
+// Видаліть перший елемент та виведіть його в консоль.
+// Вставте «Country» та «Reggae» на початок масиву.
+// const genres = ['Jazz', 'Blues'];
+// genres.push('Рок-н-рол');
+// console.log(genres.shift());
+// genres.unshift('Country', 'Reggae')
+// console.log(genres);
+// console.log(genres[genres.length - 1]);
+// const genres = ['Jazz', 'Blues'];
+// genres.splice(genres.length, 0, 'Рок-н-рол');
+// console.log(genres.splice(0, 1, 'Country', 'Reggae'));
+// console.log(genres);
+
+// Example 2 - Масиви та рядки
+// Напиши скрипт для обчислення площі прямокутника зі сторонами, значення яких зберігаються у змінній values у вигляді рядка. Значення гарантовано розділені пробілом.
+
+// const values = '8 11';
+// const arr = values.split(' ');
+// const first = Number(arr[0]);
+// const last = Number(arr[arr.length - 1]);
+// const result = first * last;
+// console.log(result);
+
+
+// Example 3 - Перебір масиву
+// Напиши скрипт для перебору масиву fruits циклом for. Для кожного елемента масиву виведи в консоль рядок у форматі номер_елемента: значення_елемента. Нумерація елементів повинна починатися з 1.
+
+const fruits = ['🍎', '🍇', '🍑', '🍌', '🍋'];
+
+// for (let i = 0; i < fruits.length; i += 1) {
+//     console.log(`${i + 1} - ${fruits[i]}`);
+// }
+
+// for (let i = 0, n = 1; i < fruits.length; i += 1, n+=1) {
+//     console.log(`${n} - ${fruits[i]}`);
+// }
+
+
+// Example 4 - Масиви та цикли
+// Напиши скрипт, який виводить у консоль ім'я та телефонний номер користувача. У змінних names та phones зберігаються рядки імен та телефонних номерів, розділені комами. Порядковий номер імен та телефонів у рядках вказують на відповідність. Кількість імен та телефонів гарантовано однакова.
+
+// let names = 'Jacob,William,Solomon,Artem';
+// let phones = '89001234567,89001112233,890055566377';
+// names = names.split(',');
+// phones = phones.split(',');
+// for (let i = 0; i < names.length, i < phones.length; i += 1){
+//     console.log(`${names[i]} - ${phones[i]}`);
+// }
+
+
+// Example 5 - Масиви та рядки
+// Напиши скрипт, який виводить у консоль усі слова рядка крім першого і останнього. Результуючий рядок не повинен починатися або закінчуватися символ пробілу. Скрипт повинен працювати для будь-якого рядка.
+
+// const string = 'Welcome to the future';
+
+// arr.pop();
+// arr.shift();
+
+// const result = arr.join(' ');
+// console.log(result);
+
+// const result = string.split(' ').slice(1, -1).join(' ');
+
+// console.log(result);
+
+// const arr = string.split(' ');
+// const test = arr.push('1234')
+// console.log(test);
+// console.log(arr);
+
+// Example 6 - Масиви та рядки
+// Напиши скрипт, який «розгортає» рядок (зворотний порядок букв) і виводить його в консоль.
+
+// const string = 'Welcome to the future';
+// const result = string.split('').reverse().join('');
+// console.log(result);
+
+
+// Example 7 - Сортування масиву із циклом
+// Напиши скрипт сортування масиву рядків в алфавітному порядку за першою літерою елемента.
+
+const langs = ['python', 'javascript', 'c++', 'haskel', 'php', 'ruby'];
+// langs.sort((a,b) => a.localeCompare(b));
+// ['c++', 'haskel', 'javascript', 'php', 'python', 'ruby']
+// console.log(langs);
+
+// нажаль наш варіант був не вірний 😥😥😥
+
+// for (let i = 0; i < langs.length; i += 1) {
+//     if (langs[0] > langs[i]) {
+//         const result = langs.splice(i,1)[0];
+//         langs.unshift(result)
+//     }
+// }
+// console.log(langs);
+
+
+// Вірні варіанти
+// 1 метод бульбашка 
+// https://www.youtube.com/watch?v=-_0dD2shabQ
+// http://xn--80adth0aefm3i.xn--j1amh/%D0%BF%D1%83%D0%B1%D0%BB%D1%96%D0%BA%D0%B0%D1%86%D1%96%D1%8F/21
+// for (let i = 0; i < langs.length; i++) {
+//     for (let j = 0; j < langs.length; j++) {
+//         if (langs[j] > langs[j + 1]) {
+//             const item = langs[j];
+//             langs[j] = langs[j + 1];
+//             langs[j + 1] = item;
+//         }
+//     }
+// }
+// console.log(langs);
+
+
+// Сучасний варіант
+// langs.sort((a,b) => a.localeCompare(b))
+// console.log(langs);
+
+
+
+
+
+
+
+// Напиши скрипт пошуку найменшого числа у масиві. Код повинен працювати для будь-якого масиву чисел. Використовуй цикл для розв'язання задачі.
+
+// const numbers = [94, 2, 17, 94, 1, 23, 37];
+// let min = numbers[0]; // undefined
+// for (const value of numbers) {
+//     if (value < min) {
+//         min = value;
+//     }
+// }
+// console.log(min); // 1
+
+// const arr = [1, 2, 3] + [4, 5];
+// console.log(arr);
+// let x = 7;
+// let y = null;
+// let a = 9;
+
+// console.log(a || x && y);
